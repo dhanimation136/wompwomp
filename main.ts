2f3c44710371af67aa16912dc7ae0a0ca4bacea9
dhanimation136 <135627786+dhanimation136@users.noreply.github.com>
2024-04-03 12:27:40 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.calibrateCompass()
+music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.UntilDone)
+basic.forever(function () {
+    basic.showNumber(1000000
+    )
+	
+})
